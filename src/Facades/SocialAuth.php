2467f1ff0c0e58e365436968730b14de5a8cbcae
foziluff <fozilovabdurazzoq@gmail.com>
2026-08-20 16:11:58 +0500

    Add IDE auto-completion to Facade

--- src/Facades/SocialAuth.php
+++ src/Facades/SocialAuth.php
@@ -4,6 +4,12 @@ namespace Foziluff\SocialAuth\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array<string, mixed>|false verifyGoogle(string $token, ?string $clientId = null)
+ * @method static array<string, mixed>|false verifyApple(string $token, ?string $clientId = null)
+ *
+ * @see \Foziluff\SocialAuth\Services\SocialAuthService
+ */
 class SocialAuth extends Facade
 {
     protected static function getFacadeAccessor(): string
